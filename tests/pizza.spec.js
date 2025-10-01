ddc72bebb04cfd1b766b16eeccb367e22f5f2f3f
david.bloomfield <david.s.bloomfield@gmail.com>
2025-09-30 19:14:15 -0600

    add login test

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -5,3 +5,15 @@ test('home page', async ({ page }) => {
 
   expect(await page.title()).toBe('JWT Pizza');
 });
+
+test('Login', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page.getByRole('heading')).toContainText('The web\'s best pizza');
+  // TODO: Assert something
+})
