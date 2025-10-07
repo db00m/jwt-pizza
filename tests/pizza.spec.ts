7cbab99d0ff059701c07bdf5ccf6b16b7dbae283
david.bloomfield <david.s.bloomfield@gmail.com>
2025-10-06 18:11:09 -0600

    add logout and regsiter tests

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -1,24 +1,11 @@
 import { test, expect } from 'playwright-test-coverage';
-import { initialize } from "./serviceMocks";
+import { initialize, initializeWithUser } from "./serviceMocks";
 
 test('homePage', async ({ page }) => {
   await page.goto('/');
 
   expect(await page.title()).toBe('JWT Pizza');
 });
-
-test('Login', async ({ page }) => {
-  await initialize(page);
-  await page.goto('/');
-  await page.getByRole('link', { name: 'Login' }).click();
-  await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('a');
-  await page.getByRole('button', { name: 'Login' }).click();
-
-  await expect(page.getByRole('heading')).toContainText('The web\'s best pizza');
-  await expect(page.locator('#navbar-dark')).toContainText('Logout');
-})
 
 test('About', async ({ page }) => {
   await page.goto('/');
@@ -30,4 +17,33 @@ test('History', async ({ page }) => {
   await page.goto('/');
   await page.getByRole('link', { name: 'History' }).click();
   await expect(page.getByRole('heading')).toContainText('Mama Rucci, my my');
+})
+
+test('register', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('link', { name: 'Register' }).click();
+  await page.getByRole('textbox', { name: 'Full name' }).click();
+  await page.getByRole('textbox', { name: 'Full name' }).fill('d');
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('j@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('agoodpassword');
+  await page.getByRole('button', { name: 'Register' }).click();
+
+  await expect(page.locator('#navbar-dark')).toContainText('Logout');
+})
+
+test('Login', async ({ page }) => {
+  await initializeWithUser(page);
+
+  await expect(page.getByRole('heading')).toContainText('The web\'s best pizza');
+  await expect(page.locator('#navbar-dark')).toContainText('Logout');
+})
+
+test('Logout', async ({ page }) => {
+  await initializeWithUser(page);
+  await page.getByRole('link', { name: 'Logout' }).click();
+
+  await expect(page.locator('#navbar-dark')).toContainText('Login');
 })
--- tests/serviceMocks.ts
+++ tests/serviceMocks.ts
@@ -11,17 +11,30 @@ export const initialize = async (page: Page) => {
   // Authorize login for the given user
   await page.route('*/**/api/auth', async (route) => {
     const loginReq = route.request().postDataJSON();
-    const user = validUsers[loginReq.email];
-    if (!user || user.password !== loginReq.password) {
-      await route.fulfill({ status: 401, json: { error: 'Unauthorized' } });
-      return;
+
+    if (route.request().method() == "PUT" || route.request().method() == "POST") {
+      const user = validUsers[loginReq.email];
+      if (!user || user.password !== loginReq.password) {
+        await route.fulfill({ status: 401, json: { error: 'Unauthorized' } });
+        return;
+      }
+      loggedInUser = validUsers[loginReq.email];
+      const loginRes = {
+        user: loggedInUser,
+        token: FAKE_TOKEN,
+      };
+      await route.fulfill({ json: loginRes });
     }
-    loggedInUser = validUsers[loginReq.email];
-    const loginRes = {
-      user: loggedInUser,
-      token: FAKE_TOKEN,
-    };
-    expect(route.request().method()).toBe('PUT');
-    await route.fulfill({ json: loginRes });
+
   });
+}
+
+export const initializeWithUser = async (page: Page) => {
+  await page.goto('/');
+  await initialize(page);
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('a');
+  await page.getByRole('button', { name: 'Login' }).click();
 }
